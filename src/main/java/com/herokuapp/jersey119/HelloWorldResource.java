fd8f49727bb6267e80aef0827a1131a1e7418f84
jurin-n <jurin.nakano@gmail.com>
2015-06-15 22:24:40 +0900

    my first commit.

---
+++ src/main/java/com/herokuapp/jersey119/HelloWorldResource.java
@@ -0,0 +1,15 @@
+package com.herokuapp.jersey119;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+//The Java class will be hosted at the URI path "/helloworld".
+@Path("/helloworld")
+public class HelloWorldResource {
+	@GET
+	@Produces("text/plain")
+	public String getClichedMessage(){
+		return "Hello World";
+	}
+}
