3dd79f06e6453bd83aa307a17325d310ca9d685b
jurin-n <jurin.nakano@gmail.com>
2015-06-17 21:55:03 +0900

    edit CustomerResourceTest.
    --detail
    add setUp method,tearDown method.

--- src/test/java/com/herokuapp/jersey119/ex04_1/CustomerResourceTest.java
+++ src/test/java/com/herokuapp/jersey119/ex04_1/CustomerResourceTest.java
@@ -2,6 +2,10 @@ package com.herokuapp.jersey119.ex04_1;
 
 import static org.junit.Assert.*;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Ignore;
 
@@ -9,7 +13,33 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 
 public class CustomerResourceTest {
+	Server server;
+	
+	@Before
+	public void setUp() throws Exception{
+		
+		System.out.println("Start server.");
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
 
+        server = new Server(Integer.valueOf(webPort));
+        final WebAppContext root = new WebAppContext();
+
+        root.setContextPath("/");
+        root.setParentLoaderPriority(true);
+
+        final String webappDirLocation = "src/main/webapp/";
+        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+        root.setResourceBase(webappDirLocation);
+
+        server.setHandler(root);
+
+        server.start();
+        //server.join();
+	}
+	
 	//@Test
 	@Ignore
 	public void testPatchMethod() {
@@ -27,6 +57,12 @@ public class CustomerResourceTest {
 		WebResource wr = c.resource("http://localhost:8080/customers/1");
 		wr.method("GET");
 		System.out.println("*** testGetMethod end ***");
+	}
+	
+	@After
+	public void tearDown() throws Exception{
+		server.stop();
+		System.out.println("Stop server.");
 	}
 
 }
