564694db4c5c066fce05469e3d84e801fa815946
jurin-n <jurin.nakano@gmail.com>
2015-06-18 21:15:26 +0900

    add Jetty Maven Plugin for test, and edit test code.

--- src/test/java/com/herokuapp/jersey119/ex04_1/CustomerResourceTest.java
+++ src/test/java/com/herokuapp/jersey119/ex04_1/CustomerResourceTest.java
@@ -1,68 +1,50 @@
 package com.herokuapp.jersey119.ex04_1;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPatch;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
+import org.junit.Test;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
-
-public class CustomerResourceTest {
-	Server server;
-	
-	@Before
-	public void setUp() throws Exception{
-		
-		System.out.println("Start server.");
-        String webPort = System.getenv("PORT");
-        if (webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
-        }
-
-        server = new Server(Integer.valueOf(webPort));
-        final WebAppContext root = new WebAppContext();
-
-        root.setContextPath("/");
-        root.setParentLoaderPriority(true);
-
-        final String webappDirLocation = "src/main/webapp/";
-        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
-        root.setResourceBase(webappDirLocation);
-
-        server.setHandler(root);
-
-        server.start();
-        //server.join();
-	}
-	
-	//@Test
-	@Ignore
+public class CustomerResourceTest {	
+	@Test
 	public void testPatchMethod() {
 		System.out.println("*** testPatchMethod start ***");
-		Client c = Client.create();
-		WebResource wr = c.resource("http://localhost:8080/customers/1");
-		wr.method("PATCH");
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		HttpPatch method = new HttpPatch("http://localhost:8080/customers/1");
+		try {
+			HttpResponse response = httpClient.execute(method);
+			assertThat(response.getStatusLine().getStatusCode(),is(204));
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
 		System.out.println("*** testPatchMethod end ***");
 	}
 	
 	@Test
 	public void testGetMethod() {
 		System.out.println("*** testGetMethod start ***");
-		Client c = Client.create();
-		WebResource wr = c.resource("http://localhost:8080/customers/1");
-		wr.method("GET");
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		HttpGet method = new HttpGet("http://localhost:8080/customers/1");
+		try {
+			HttpResponse response = httpClient.execute(method);
+			assertThat(response.getStatusLine().getStatusCode(),is(200));
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
 		System.out.println("*** testGetMethod end ***");
 	}
-	
-	@After
-	public void tearDown() throws Exception{
-		server.stop();
-		System.out.println("Stop server.");
-	}
-
 }
