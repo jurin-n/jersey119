260e0088c341907ca71e3c5537fccafa817e1cf5
jurin-n <jurin.nakano@gmail.com>
2015-06-23 22:29:03 +0900

    add PathSegment Samples.

--- src/main/java/com/herokuapp/jersey119/ex05_1/CarResource.java
+++ src/main/java/com/herokuapp/jersey119/ex05_1/CarResource.java
@@ -1,11 +1,12 @@
 package com.herokuapp.jersey119.ex05_1;
 
+import java.util.List;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.MatrixParam;
-
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.PathSegment;
 
@@ -31,4 +32,31 @@ public class CarResource {
 		return "A " + color + " " + year + " " + make + " " + car.getPath();
 	}
 	
+	@GET
+	@Path("/segment/{make}/{model}/{year}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getFromPathSegment(
+			@PathParam("make") String make,
+			@PathParam("model") PathSegment car,
+			@PathParam("year") String year
+			){
+		
+		String carColor = car.getMatrixParameters().getFirst("color");
+		return  "A " + carColor + " " + year + " " + make + " " + car.getPath();
+	}
+	
+	@GET
+	@Path("/segments/{make}/{model : .+}/{year}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getFromPathSegments(
+			@PathParam("make") String make,
+			@PathParam("model") List<PathSegment> car,
+			@PathParam("year") String year
+			){
+		String res = "A " + year + " " + make;
+		for(PathSegment seg : car){
+			res = res + " " + seg.getPath() + " " +  seg.getMatrixParameters().getFirst("color");
+		}
+		return res;
+	}
 }
--- src/test/java/com/herokuapp/jersey119/ex05_1/CarResourceTest.java
+++ src/test/java/com/herokuapp/jersey119/ex05_1/CarResourceTest.java
@@ -12,6 +12,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.junit.Ignore;
+import org.junit.Test;
 
 public class CarResourceTest {
 
@@ -39,5 +40,45 @@ public class CarResourceTest {
 			fail();
 		}
 	}
+	
+	@Test
+	public void testPathSegmentの動作を確認する() {
+		try {
+			HttpClient httpClient = HttpClientBuilder.create().build();
+			
+			System.out.println("GET http://localhost:8080/ex05_1/cars/segment/mercedes/e55;color=black/2015");
+			HttpGet method = new HttpGet("http://localhost:8080/ex05_1/cars/segment/mercedes/e55;color=black/2015");
+			HttpResponse res = httpClient.execute(method);
+			assertThat(
+					res.getStatusLine().getStatusCode()
+					,is(HttpURLConnection.HTTP_OK));
+			assertThat(
+					EntityUtils.toString(res.getEntity(), "UTF-8")
+					,is("A black 2015 mercedes e55"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
 
+	@Test
+	public void testPathSegmentのListの動作を確認する() {
+		try {
+			HttpClient httpClient = HttpClientBuilder.create().build();
+			
+			System.out.println("GET http://localhost:8080/ex05_1/cars/segments/mercedes/e55;color=black/x1;color=red/2015");
+			HttpGet method = new HttpGet("http://localhost:8080/ex05_1/cars/segments/mercedes/e55;color=black/x1;color=red/2015");
+			HttpResponse res = httpClient.execute(method);
+			assertThat(
+					res.getStatusLine().getStatusCode()
+					,is(HttpURLConnection.HTTP_OK));
+			assertThat(
+					EntityUtils.toString(res.getEntity(), "UTF-8")
+					,is("A 2015 mercedes e55 black x1 red"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
 }
