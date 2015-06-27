34ee0b388d3a3561b2f79fcb996607b3e99205dd
jurin-n <jurin.nakano@gmail.com>
2015-06-27 17:31:32 +0900

    add Xml Support Sample codes and test code.

--- src/main/java/com/herokuapp/jersey119/ex06_1/Planet.java
+++ src/main/java/com/herokuapp/jersey119/ex06_1/Planet.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement
+@XmlRootElement(name="planet")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Planet {
 	@XmlAttribute
--- src/main/java/com/herokuapp/jersey119/ex06_1/PlanetResource.java
+++ src/main/java/com/herokuapp/jersey119/ex06_1/PlanetResource.java
@@ -1,6 +1,8 @@
 package com.herokuapp.jersey119.ex06_1;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -17,4 +19,12 @@ public class PlanetResource {
 		return p;
 	}
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
+	public Planet setPlanet(Planet p){
+		System.out.println("getId=" + p.getId());
+		System.out.println("getName=" + p.getName());
+		System.out.println("getRadius=" + p.getRadius());
+		return p;
+	}
 }
